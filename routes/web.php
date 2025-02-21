00c5789b40bbc5e81c0a96675425d7c0b3051c58
cputrief <cutputriefrina@gmail.com>
2025-02-21 10:38:41 +0700

    route part 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+// use = sama dengan include
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +12,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// halaman domain
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/cth', function () {
+//     return view('contoh');
+// });
+// // untuh halaman yang tidak ada pakai fallback
+// Route::fallback( function () {
+//     return view('notfound');
+// });
+// // nama itu sama dengan id untuk di panggi dari url nya
+// Route::get('mahasiswa/{nama}', function ($nama) {
+//     echo "ini halaman mahasiswa dengan nama $nama";
+// });
+
