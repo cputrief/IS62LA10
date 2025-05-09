4ada2c38743df7b6686a2aa9bc4b8fc9121792c8
cputrief <cutputriefrina@gmail.com>
2025-05-09 11:48:04 +0700

    buat table dan form

--- resources/views/Dosen/index.blade.php
+++ resources/views/Dosen/index.blade.php
@@ -5,10 +5,38 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Data Dosen') }}
+                    <a href="dosen/tambah"><button type="button" class="btn btn-primary float-end">Tambah Data</button></a>
+                </div>
 
                 <div class="card-body">
-                   ini manampilkan data dosen
+                   
+                   <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">No</th>
+                        <th scope="col">NIDN</th>
+                        <th scope="col">Nama Dosen</th>
+                        <th scope="col">Jabatan</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">No HP</th>
+                        <th scope="col">Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">1</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                       
+                      </tr>
+                      
+                    </tbody>
+                  </table>
 
                     {{ __('You are logged in!') }}
                 </div>
